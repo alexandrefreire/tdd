290bfcfcf13cf8942c6fa2c18992477676aee60a
alex <alex@industriallogic.com>
2018-01-19 11:43:18 -0800

    it's Pi and (c)

--- src/main/main.go
+++ src/main/main.go
@@ -1,5 +1,15 @@
+/* ***************************************************************************
+# Copyright (c) 2018, Industrial Logic, Inc., All Rights Reserved.
+#
+# This code is the exclusive property of Industrial Logic, Inc. It may ONLY be
+# used by students during Industrial Logic's workshops or by individuals
+# who are being coached by Industrial Logic on a project.
+#
+# This code may NOT be copied or used for any other purpose without the prior
+# written consent of Industrial Logic, Inc.
+# ****************************************************************************/
 package main
 
 func DoSomething() float32 {
-	return 3.412
+	return 3.14
 }
--- src/main/main_test.go
+++ src/main/main_test.go
@@ -1,9 +1,19 @@
+/* ***************************************************************************
+# Copyright (c) 2018, Industrial Logic, Inc., All Rights Reserved.
+#
+# This code is the exclusive property of Industrial Logic, Inc. It may ONLY be
+# used by students during Industrial Logic's workshops or by individuals
+# who are being coached by Industrial Logic on a project.
+#
+# This code may NOT be copied or used for any other purpose without the prior
+# written consent of Industrial Logic, Inc.
+# ****************************************************************************/
 package main
 
 import "testing"
 
 func TestCompareToSelf(t *testing.T) {
-	if 3.412 != DoSomething() {
+	if 3.14 != DoSomething() {
 		t.Fail()
 	}
 }
